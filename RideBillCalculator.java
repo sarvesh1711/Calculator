6127724ae52416f937af9c402a09667c0126ddb0
Sarvesh Sonawane <Sarvesh.Sonawane@asl.mum.sst>
2023-07-06 23:45:08 +0530

    cal

---
+++ RideBillCalculator.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class RideBillCalculator
+{
+public static void main(String args[])
+{
+
+Scanner sc=new Scanner(System.in);
+System.out.println("enter height");
+int height=sc.nextInt();
+
+if(height>120)
+{
+System.out.println("Can ride");
+}
+else
+{
+System.out.println("Can't ride");
+}
+System.out.println("enter age");
+int age=sc.nextInt();
+if(age<12)
+{
+System.out.println("$5");
+}
+else if(age>=12 && age<=18)
+{
+	System.out.println("$7");
+}
+else if(age>=18)
+{
+	System.out.println("$12");
+}
+else if(age>=45 && age<=55)
+{
+	System.out.println("$0");
+}
+System.out.println("want photos?");
+String wantPhotos=sc.next();
+	switch(wantPhotos)
+	{
+	case "Yes":
+	System.out.println("+$3");
+	break;
+	
+	case "No":
+	System.out.println("the total bill is $x");
+	break;
+	
+}
+}
+}
+
+
