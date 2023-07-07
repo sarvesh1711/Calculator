8b7dfd4ce071ffe0f1bdf2a65c08646372420359
sarvesh1711 <53569173+sarvesh1711@users.noreply.github.com>
2023-07-07 15:40:41 +0530

    Update RideBillCalculator.java

--- RideBillCalculator.java
+++ RideBillCalculator.java
@@ -1,66 +1,77 @@
-import java.util.Scanner;
-class RideBillCalculator
-{
-public static void main(String args[])
-{
+import java.util.Scanner; 
+class RideBillCalculator{ 
+    public static void main(String[] args){ 
+        Scanner sc = new Scanner(System.in); 
 
-Scanner sc=new Scanner(System.in);
-System.out.println("enter height");
-int height=sc.nextInt();
-
-if(height>120)
-{
-System.out.println("Can ride");
+        System.out.println("Enter your Height in cm : "); 
+        int height=sc.nextInt(); 
+        int photoPrice = 3; 
+        if(height<120){ 
+            System.out.println("Sorry you are not eligible ride"); 
+        } 
+        else{ 
+            System.out.println("You can ride !!"); 
+            System.out.println("Enter your age : "); 
+            int age=sc.nextInt(); 
+            if(age<12){ 
+                System.out.println("For photos you will have to pay 3$ extra!!"); 
+                System.out.println("Do you want to take photos : "); 
+                String wantPhotos=sc.next(); 
+                if(wantPhotos.equalsIgnoreCase("no")){ 
+                    System.out.println("your ride bill is 5$"); 
+                } 
+                else if(wantPhotos.equalsIgnoreCase("yes")){ 
+                    System.out.println("your ride bill is "+(5+photoPrice)+"$"); 
+                } 
+                else{ 
+                    System.out.println("Invalid entry !!"); 
+                } 
+            } 
+            else if(age>=12 && age <18){ 
+                System.out.println("For photos you will have to pay 3$ extra!!"); 
+                System.out.println("Do you want to take photos : "); 
+                String wantPhotos=sc.next(); 
+                if(wantPhotos.equalsIgnoreCase("no")){ 
+                    System.out.println("your ride bill is 7$"); 
+                } 
+                else if(wantPhotos.equalsIgnoreCase("yes")){ 
+                    System.out.println("your ride bill is "+(7+photoPrice)+"$"); 
+                } 
+                else{ 
+                    System.out.println("Invalid entry !!"); 
+                } 
+            } 
+            else if(age>=18 && age <45){ 
+                System.out.println("For photos you will have to pay 3$ extra!!"); 
+                System.out.println("Do you want to take photos : "); 
+                String wantPhotos=sc.next(); 
+                if(wantPhotos.equalsIgnoreCase("no")){ 
+                    System.out.println("your ride bill is 12$"); 
+                } 
+                else if(wantPhotos.equalsIgnoreCase("yes")){ 
+                    System.out.println("your ride bill is "+(12+photoPrice)+"$"); 
+                } 
+                else{ 
+                    System.out.println("Invalid entry !!"); 
+                } 
+            } 
+            else if(age>=45 && age <=55){ 
+                System.out.println("For photos you will have to pay 3$ extra!!"); 
+                System.out.println("Do you want to take photos : "); 
+                String wantPhotos=sc.next(); 
+                if(wantPhotos.equalsIgnoreCase("no")){ 
+                    System.out.println("your ride bill is 0$"); 
+                } 
+                else if(wantPhotos.equalsIgnoreCase("yes")){ 
+                    System.out.println("your ride bill is "+(0+photoPrice)+"$"); 
+                } 
+                else{ 
+                    System.out.println("Invalid entry !!"); 
+                } 
+            } 
+            else{ 
+                System.out.println("considering your age you are not eligible for ride"); 
+            } 
+        } 
+    } 
 }
-else
-{
-System.out.println("Can't ride");
-}
-System.out.println("enter age");
-int age=sc.nextInt();
-if(age<12)
-{
-System.out.println("$5");
-}
-else if(age>=12 && age<=18)
-{
-	System.out.println("$7");
-}
-else if(age>=18)
-{
-	System.out.println("$12");
-}
-else if(age>=45 && age<=55)
-{
-	System.out.println("$0");
-}
-System.out.println("want photos?");
-String wantPhotos=sc.next();
-	switch(wantPhotos)
-	{
-	case "Yes":
-	System.out.println("+$3");
-	break;
-	
-	case "No":
-	System.out.println("the total bill is $x");
-	break;
-	
-}
-}
-}
-
-Output:
-
-C:\Users\sarvesh.sonawane\Desktop\Calculator>javac RideBillCalculator.java
-
-C:\Users\sarvesh.sonawane\Desktop\Calculator>java RideBillCalculator
-enter height
-121
-Can ride
-enter age
-13
-$7
-want photos?
-Yes
-+$3
